30af77cbe3b6f6c435fe35fe042c776eb6beb987
thearraspolice <157838180+thearraspolice@users.noreply.github.com>
2024-07-04 10:11:54 -0600

    Initialize entity-designer

---
+++ server/lib/definitions.js
@@ -0,0 +1,12 @@
+/*
+    THE DEFINITIONS YOU'RE LOOKING FOR ARE IN ANOTHER CASTLE!
+    definitions.js has been migrated to server/modules/definitions, please use that instead
+*/
+console.error(`
+/------------------------------------------------------------------------------------/
+
+THE DEFINITIONS YOU'RE LOOKING FOR ARE IN ANOTHER CASTLE!
+definitions.js has been migrated to server/modules/definitions, please use that instead
+
+/------------------------------------------------------------------------------------/
+`)
